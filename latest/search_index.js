6b44a9962cac2e50ba1affcaeb483688e8d688b9
autodocs <autodocs>
2017-11-19 08:44:13 +0000

    build based on 26bd463

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "NPFinancial.jl Documentation",
     "category": "section",
-    "text": "NPFinancial.jl is a port of the NumPy's financial module. "
+    "text": ""
 },
 
 {
