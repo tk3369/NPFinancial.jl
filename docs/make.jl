90b6f8034377ff96dc21495f1e20b85ebf11bc05
Tom Kwong <tk3369@gmail.com>
2020-02-09 00:09:50 -0800

    removed versions settting (use defualt)

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,6 @@ makedocs(
 
 deploydocs(
     repo = "github.com/tk3369/NPFinancial.jl.git",
-    versions = ["stable" => "v^"],
     deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     make   = () -> run(`mkdocs build`),
     target = "site"
